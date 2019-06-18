9ac7aa333eb315906afd1693ad006afb1a2ad4a5
Jack Kelly <jgkelly@mit.edu>
2019-06-18 10:21:07 -0400

    Place pawns

--- frontend/board.js
+++ frontend/board.js
@@ -1,11 +1,14 @@
 function make_board() {
-    console.log("hello")
     var table = document.createElement("table");
     table.className = "board";
-    for (var i = 1; i < 9; i++) {
+    for (var i = 0; i < 8; i++) {
         var tr = document.createElement('tr');
-        for (var j = 1; j < 9; j++) {
+        for (var j = 0; j < 8; j++) {
             var td = document.createElement('td');
+            td.id = j.toString() + "_" + (7 - i).toString();
+            td.onclick = place_pawn;
+            // td.addEventListener("click", place_pawn(td));
+
             if (i%2 == j%2) {
                 td.className = "white";
             } else {
@@ -15,9 +18,32 @@ function make_board() {
         }
         table.appendChild(tr);
     }
-    var div = document.createElement("div")
-    div.id = "test"
-    div.appendChild(table)
+    var div = document.createElement("div");
+    div.id = "outer_box";
+    div.appendChild(table);
     document.body.appendChild(div);
 }
 window.onload = make_board;
+
+
+function place_pawn(click_event) {
+
+    var td = click_event["path"][0];
+
+    var pawn_elem = document.createElement("img");
+    pawn_elem.className = "piece_image";
+
+    if (parseInt(td.id.slice(-1)) < 4){
+        pawn_elem.src = "white_pawn.png";
+    }
+    else{
+        pawn_elem.src = "black_pawn.png";
+
+    }
+    var div = document.createElement("div");
+    div.className = "square";
+    div.appendChild(pawn_elem);
+    td.appendChild(div);
+}
+
+
